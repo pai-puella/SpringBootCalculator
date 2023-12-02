ff93286b698c872ed188347aa4df38e48c23b0e4
pai-puella <cat.dyshkant@yandex.ru>
2023-12-02 02:43:36 -0800

    initial commit

---
+++ src/main/java/com/calculator/core/CoreApplication.java
@@ -0,0 +1,16 @@
+package com.calculator.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.*;
+
+@SpringBootApplication
+@RestController
+@RequestMapping("/calculator")
+public class CoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreApplication.class, args);
+	}
+
+}
